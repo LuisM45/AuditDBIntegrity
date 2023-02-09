6d7872c4ce0c58b2badb6228c9d4b93682456e08
luism <luism@DESKTOP-KVGUO67>
2023-02-08 23:53:06 -0500

    Debug line removed

--- src/main/java/edu/epn/aud/b2022/g1/integrityScanner/Main.java
+++ src/main/java/edu/epn/aud/b2022/g1/integrityScanner/Main.java
@@ -11,21 +11,7 @@ public class Main {
     static Function<Object,Object> identity = t->t;
     
     public static void main(String[] args) {
-        try {
-            DBConection dbc = DBConection.getConnection("127.0.0.1", "1433", "pubs", "sa", "win10123!");
-            Stream.of(
-                    dbc.getConstraints(),
-                    dbc.getDatefulAnomalies(),
-                    dbc.getDatelessAnomalies_(),
-                    dbc.getTriggers_()
-                ).flatMap(t->t.stream())
-                .forEach(System.out::println);
-            
-            System.out.println();
-        } catch (SQLException ex) {
-            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-        }
-//        new GUILogin().setVisible(true);        
+        new GUILogin().setVisible(true);        
     }
     
 }
