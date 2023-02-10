7b6a237454d9111152636075f4e23b8e8e5a347f
luism <luism@DESKTOP-KVGUO67>
2023-02-09 21:56:26 -0500

    Better exceptions handles. Introduced error dialog messages

--- src/main/java/edu/epn/aud/b2022/g1/integrityScanner/Main.java
+++
@@ -1,9 +0,0 @@
-package edu.epn.aud.b2022.g1.integrityScanner;
-
-public class Main {
-    
-    public static void main(String[] args) {
-        new GUILogin().setVisible(true);        
-    }
-    
-}
