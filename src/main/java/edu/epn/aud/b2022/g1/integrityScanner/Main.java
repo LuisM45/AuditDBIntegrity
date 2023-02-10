e56628ed8119851de17e13f147c401bbd435168a
luism <luism@DESKTOP-KVGUO67>
2023-02-09 20:59:47 -0500

    Removed unused code/imports

--- src/main/java/edu/epn/aud/b2022/g1/integrityScanner/Main.java
+++ src/main/java/edu/epn/aud/b2022/g1/integrityScanner/Main.java
@@ -1,14 +1,6 @@
 package edu.epn.aud.b2022.g1.integrityScanner;
 
-import java.sql.SQLException;
-import java.util.function.Function;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import java.util.stream.Stream;
-
 public class Main {
-
-    static Function<Object,Object> identity = t->t;
     
     public static void main(String[] args) {
         new GUILogin().setVisible(true);        
